2987b740b8876b5cc3034cbc64665900e544d141
Shital2005 <shitalvijayprajapati85@gmail.com>
2024-10-07 22:39:45 +0530

    Mongo File

---
+++ attendance_mongo/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import EmployeeForm from './components/EmployeeForm'; // Ensure the correct path to EmployeeForm component
+
+const App = () => {
+  return (
+    <div>
+      <h1>Employee Registration</h1>
+      <EmployeeForm />
+    </div>
+  );
+};
+
+export default App; // Ensure App is exported as default
+
