e229254f65b656ed420511ea285acb02f8f8cb15
OmMoholkar <omoholkar30@gmail.com>
2024-10-10 20:21:03 +0530

    changed path to login.php

--- AppTest/app/src/main/java/com/example/medimetrics/data/network/ApiClient.kt
+++ AppTest/app/src/main/java/com/example/medimetrics/data/network/ApiClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    private const val BASE_URL = "http://10.0.2.2/MediMetrics/attendance/"  // For local XAMPP, use 10.0.2.2 on emulator
+    private const val BASE_URL = "http://10.0.2.2/MediMetrics/website/"  // For local XAMPP, use 10.0.2.2 on emulator
 
     val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
